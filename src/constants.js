791ad83ea341b979ea9c19582cd92d013fc4bc7b
Shivam Dubey <118355740+shivam969619@users.noreply.github.com>
2024-08-25 19:19:02 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const USER_END_POINT = "http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job";
+export const USER_END_POINT = "https://unstopbackend-ih08.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://unstopbackend-ih08.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company";
+  "https://unstopbackend-ih08.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://unstopbackend-ih08.onrender.com//api/v1/company";
